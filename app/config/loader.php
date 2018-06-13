ebda36feb336ca15f208cf764d8d1aeca85ffcc4
Scott Huson <randomrandom623@gmail.com>
2018-06-13 22:20:36 +0800

    CMS 3

--- app/config/loader.php
+++ app/config/loader.php
@@ -6,7 +6,7 @@ $loader = new \Phalcon\Loader();
 $loader->registerDirs([
     APP_PATH . $config->application->controllersDir,
     //APP_PATH . $config->application->pluginsDir,
-    //APP_PATH . $config->application->libraryDir,
+    APP_PATH . $config->application->libraryDir,
     //APP_PATH . $config->application->modelsDir,
     //APP_PATH . $config->application->formsDir
 ])->register();
