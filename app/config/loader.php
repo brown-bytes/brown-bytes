ad6e89965ae4b74a83e74f6d534551c8084e9d98
Scott Huson <randomrandom623@gmail.com>
2018-06-13 22:13:15 +0800

    CMS 2

--- app/config/loader.php
+++ app/config/loader.php
@@ -5,10 +5,10 @@ $loader = new \Phalcon\Loader();
  */
 $loader->registerDirs([
     APP_PATH . $config->application->controllersDir,
-    APP_PATH . $config->application->pluginsDir,
-    APP_PATH . $config->application->libraryDir,
-    APP_PATH . $config->application->modelsDir,
-    APP_PATH . $config->application->formsDir
+    //APP_PATH . $config->application->pluginsDir,
+    //APP_PATH . $config->application->libraryDir,
+    //APP_PATH . $config->application->modelsDir,
+    //APP_PATH . $config->application->formsDir
 ])->register();
 $loader->registerClasses([
     'Services' => APP_PATH . 'app/Services.php'
